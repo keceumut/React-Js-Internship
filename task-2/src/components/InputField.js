2173271171334978f2ef67287363c7b01f99bfcd
Umutcan KEÇE <keceumut15@gmail.com>
2021-07-13 17:25:33 +0300

    Get Input Values with Hooks
    Check InputGroups and disable Login button

--- task-2/src/components/InputField.js
+++ task-2/src/components/InputField.js
@@ -1,21 +1,33 @@
+import { useState } from 'react'
 import { InputGroup,FormControl,Button,Container,Row ,Col} from 'react-bootstrap'
 export default function InputFields(){
+
+    const [username,setUsername] = useState(null);
+    const [password,setPassword] = useState(null);
+
+    function checkFields(){
+        if(username == null || password==null) return true;
+        else return false;
+    }
+
     return(
         <Container fluid='lg'>           
             <Row className='justify-content-center'>                
                 <Col md='5' >
                     <InputGroup className='mb-3' size='lg'>
-                        <FormControl placeholder ='Username' ></FormControl>
+                        <FormControl placeholder ='Username' onChange={e=>setUsername(e.target.value)}></FormControl>
                     </InputGroup>           
             
                     <InputGroup className='mb-3' size='lg'>
-                        <FormControl placeholder='Password'></FormControl>
+                        <FormControl placeholder='Password' onChange={e=>setPassword(e.target.value)}></FormControl>
                     </InputGroup>
                     <Button 
                         variant='primary' 
                         className='lg-button' 
                         size='lg'
+                        disabled ={checkFields()}
                         block
+                        onClick={()=>console.log(username,password)}
                     >Log-In
                     </Button>
                 </Col>                
